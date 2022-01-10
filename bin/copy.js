05c9575ec215ad63a9d34c6eb7cf145103e6aa70
Andrey Popov <codenomnom@gmail.com>
2022-01-10 22:07:30 +0200

    remove faulty copy folder

--- bin/copy.js
+++ bin/copy.js
@@ -2,5 +2,5 @@ const fs = require('fs-extra');
 
 fs.emptyDirSync('./docs');
 fs.copySync('./src/images/', './docs/images/')
-fs.copySync('./src/scripts/', './docs/scripts/')
+// fs.copySync('./src/scripts/', './docs/scripts/')
 fs.copySync('./src/index.html', './docs/index.html')
